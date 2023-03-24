11774a9e354af75b2b603f911584ab49d2ba55ad
dineshmishra2019 <50823368+dineshmishra2019@users.noreply.github.com>
2023-03-24 12:02:09 +0530

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,30 +1,36 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.prefix.id}-rg"
-  location = "West US 2"
+resource "azurerm_resource_group" "icx-rg" {
+  name     = "ICX-RG-TERRAFORM"
+  location = "Central India"
 
   tags = {
     environment = "Demo"
   }
 }
+resource "azurerm_container_registry" "icx-acr" {
+  name                = "icxcontainerRegistry"
+  resource_group_name = azurerm_resource_group.icx-rg.name
+  location            = azurerm_resource_group.icx-rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
 
-resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
+resource "azurerm_kubernetes_cluster" "icx-aks" {
+  name                = "icx-aks-cluster"
+  location            = azurerm_resource_group.icx-rg.location
+  resource_group_name = azurerm_resource_group.icx-rg.name
+  dns_prefix          = "icx-aks-k8s-cluster"
 
   default_node_pool {
     name            = "default"
-    node_count      = 2
+    node_count      = 1
     vm_size         = "Standard_B2s"
     os_disk_size_gb = 30
   }
